a92a7bcbe6a66b1ad74fc04d7f35a5eb9b810eae
Andrew Duong <duongsters@gmail.com>
2019-09-16 19:33:27 -0700

    created the on click handler for the submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,8 +10,46 @@ var firebaseConfig = {
 };
 //—————————Initialize Firebase—————————//
 firebase.initializeApp(firebaseConfig);
+//created 'database' variable instance from the firebase object for reference back to the firebase database service
 var database = firebase.database();
 // Create and set global variable 'rendering'
 var rendering;
 
 
+
+
+//references on the new value changes then pulls new data to the firebase database
+database.ref().on("value", function(snapshot){
+//'data' was made to extract the javascript values of the Datasnapshot...using .val() would return a datasnapshot value of a scalar type 
+    var data = snapshot.val();
+
+});
+
+
+
+//captures the user's button clicks and this funcation is ran
+$("#submitBtn").on("click", function(){
+    //initally the function would collect the unique IDs created from the form area of the page with the index.html
+    var trainName = $("#trainName").val().trim();
+    var trainDestination = $("#trainDestination").val().trim();
+    var trainNextArrival = $("#trainNextArrival").val().trim();
+    var trainFrequency = $("#trainFrequency").val().trim();
+
+
+    if (trainName == "" || trainName == null) {
+        alert("Enter in a valid name of your train");
+        return false;
+    }
+    if (trainDestination == "" || trainDestination == null) {
+        alert("Enter a valid destination")
+        return false;
+    }
+    if (trainNextArrival == "" || trainNextArrival== null) {
+        alert("Enter a valid arrival time");
+        return false;
+    }
+    if (trainFrequency == "" || trainFrequency == null) {
+        alert("Enter a valid frequecy of the next arrival of your train...must be greater than 0!");
+        return false;
+    }
+})
