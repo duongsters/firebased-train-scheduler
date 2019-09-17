de92ce9714651a85c32306cd71ef6911ac4796fa
Andrew Duong <duongsters@gmail.com>
2019-09-16 18:56:45 -0700

    create and initialized global variable and created shortcuts...also created the code to ref back to the firebase server whenever the values are changed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,17 @@
-  //————My web app's Firebase configuration————//
-  var firebaseConfig = {
-    apiKey: "AIzaSyB8nr-CeZjg1ReOgEokzW08PHLiewGrcT0",
-    authDomain: "sampletest-e5e84.firebaseapp.com",
-    databaseURL: "https://sampletest-e5e84.firebaseio.com",
-    projectId: "sampletest-e5e84",
-    storageBucket: "sampletest-e5e84.appspot.com",
-    messagingSenderId: "847545129846",
-    appId: "1:847545129846:web:81183255542e47e31ef857"
-  };
-  //—————————Initialize Firebase—————————//
-  firebase.initializeApp(firebaseConfig);
+//————My web app's Firebase configuration————//
+var firebaseConfig = {
+    apiKey: "AIzaSyB8nr-CeZjg1ReOgEokzW08PHLiewGrcT0",
+    authDomain: "sampletest-e5e84.firebaseapp.com",
+    databaseURL: "https://sampletest-e5e84.firebaseio.com",
+    projectId: "sampletest-e5e84",
+    storageBucket: "sampletest-e5e84.appspot.com",
+    messagingSenderId: "847545129846",
+    appId: "1:847545129846:web:81183255542e47e31ef857"
+};
+//—————————Initialize Firebase—————————//
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+// Create and set global variable 'rendering'
+var rendering;
+
+
